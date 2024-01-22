66e55233a89b4fac778f71b33a13d63c565d5b49
Talha Ibne Mahmud <14072446-talha057@users.noreply.gitlab.com>
2024-01-22 13:15:03 +0600

    🚀 Configured: Shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <main className="min-h-svh grid place-content-center">
       <h1 className="text-5xl font-semibold">Hello World!</h1>
+
+      <section className="my-4 py-2 border border-zinc-300 rounded-lg flex flex-col items-center gap-2">
+        <p className="font-mono">{count}</p>
+        <Button onClick={() => setCount((c) => c + 1)}>Increment</Button>
+      </section>
     </main>
   );
 }
