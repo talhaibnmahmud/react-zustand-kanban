e7ec29e48072906ae1e7534be086318796207e30
Talha Ibne Mahmud <14072446-talha057@users.noreply.gitlab.com>
2024-01-22 17:09:45 +0600

    🚀 Configured: Custom middleware for logging

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,4 +1,5 @@
 import { mountStoreDevtool } from "simple-zustand-devtools";
+import type { StateCreator, StoreMutatorIdentifier } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 import { shallow } from "zustand/shallow";
 import { createWithEqualityFn } from "zustand/traditional";
@@ -19,26 +20,60 @@ type TastStore = {
   moveTask: (task: Task) => void;
 };
 
+type Logger = <
+  T,
+  Mps extends [StoreMutatorIdentifier, unknown][] = [],
+  Mcs extends [StoreMutatorIdentifier, unknown][] = []
+>(
+  f: StateCreator<T, Mps, Mcs>,
+  name?: string
+) => StateCreator<T, Mps, Mcs>;
+
+type LoggerImpl = <T>(
+  f: StateCreator<T, [], []>,
+  name?: string
+) => StateCreator<T, [], []>;
+
+const loggerImpl: LoggerImpl = (f, name) => (set, get, store) => {
+  //   type T = ReturnType<typeof f>;
+  const loggedSet: typeof set = (...a) => {
+    set(...a);
+    console.log(...(name ? [`${name}:`] : []), get());
+  };
+  store.setState = loggedSet;
+
+  return f(loggedSet, get, store);
+};
+
+export const logger = loggerImpl as unknown as Logger;
+
 export const useStore = createWithEqualityFn<TastStore>()(
   devtools(
     persist(
-      (set) => ({
-        tasks: [],
-        draggedTask: null,
-        addTask: (task) =>
-          set((store) => ({ tasks: [...store.tasks, task] }), false, "addTask"),
-        deleteTask: (title) =>
-          set((store) => ({
-            tasks: store.tasks.filter((task) => task.title !== title),
-          })),
-        setDraggedTask: (title) => set(() => ({ draggedTask: title })),
-        moveTask: ({ title, status }: Task) =>
-          set((store) => ({
-            tasks: store.tasks.map((task) =>
-              task.title === title ? { title, status } : task
+      logger(
+        (set) => ({
+          tasks: [],
+          draggedTask: null,
+          addTask: (task) =>
+            set(
+              (store) => ({ tasks: [...store.tasks, task] }),
+              false,
+              "addTask"
             ),
-          })),
-      }),
+          deleteTask: (title) =>
+            set((store) => ({
+              tasks: store.tasks.filter((task) => task.title !== title),
+            })),
+          setDraggedTask: (title) => set(() => ({ draggedTask: title })),
+          moveTask: ({ title, status }: Task) =>
+            set((store) => ({
+              tasks: store.tasks.map((task) =>
+                task.title === title ? { title, status } : task
+              ),
+            })),
+        }),
+        "Logger Middleware"
+      ),
       { name: "tastStore" }
     )
   ),
