d76f80b1dc97373b1ed6fca3ba2c5944f416fb72
Talha Ibne Mahmud <14072446-talha057@users.noreply.gitlab.com>
2024-01-22 18:40:48 +0600

    🧼 Update: Item add tested

--- __test__/store.test.tsx
+++ __test__/store.test.tsx
@@ -8,11 +8,13 @@ test("sample", () => {
   expect(1).toEqual(1);
 });
 
+type TaskStorePartial = Partial<{ [K in keyof TaskStore]: TaskStore[K] }>;
+
 const TestComponent = ({
   selector,
   effect,
 }: {
-  selector: (store: TaskStore) => TaskStore[keyof TaskStore];
+  selector: (store: TaskStore) => TaskStore[keyof TaskStore] | TaskStorePartial;
   effect: Mock;
 }) => {
   const items = useStore(selector);
@@ -30,10 +32,24 @@ test("Should return default value at the start", () => {
   expect(effect).toHaveBeenCalledWith([]);
 });
 
-// test("Should add an item to the store and rerun the effect", () => {
-//   const selector = (store: TaskStore) => store.addTask;
-//   const effect = vi.fn();
+test("Should add an item to the store and rerun the effect", () => {
+  const selector = (store: TaskStore) => ({
+    tasks: store.tasks,
+    addTask: store.addTask,
+  });
+  const effect = vi
+    .fn()
+    .mockImplementation((items: ReturnType<typeof selector>) => {
+      if (items.tasks.length === 0) {
+        items.addTask({ title: "Test Task", status: "PLANNED" });
+      }
+    });
 
-//   render(<TestComponent selector={selector} effect={effect} />);
-//   expect(effect).toHaveBeenCalledWith([]);
-// });
+  render(<TestComponent selector={selector} effect={effect} />);
+  expect(effect).toHaveBeenCalledTimes(2);
+  expect(effect).toHaveBeenCalledWith(
+    expect.objectContaining({
+      tasks: [{ title: "Test Task", status: "PLANNED" }],
+    })
+  );
+});
